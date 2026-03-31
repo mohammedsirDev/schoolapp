e57ffc33e63db93649f44251ac4c6ede06b88afe
mohammedsirDev <mohammed.sir@uit.ac.ma>
2026-03-31 06:15:12 +0200

    chore: use env variable for API

--- frontend/school/src/utils/api.js
+++ frontend/school/src/utils/api.js
@@ -1,3 +1,8 @@
-export const API_URL = process.env.REACT_APP_API_URL;
+// This tells React to look for the secret variable in Vercel
+const API_URL = process.env.REACT_APP_API_URL;
+
+if (!API_URL) {
+  console.warn("WARNING: REACT_APP_API_URL is not defined! Check Vercel Settings.");
+}
 
 export default API_URL;

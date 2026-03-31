ef4b2632746247c97c6146fe8b45dc44a6a28153
Mohammed sir <mohammed.sir@uit.ac.ma>
2026-03-31 03:25:24 +0200

    Update Register.js

--- frontend/school/src/pages/Register.js
+++ frontend/school/src/pages/Register.js
@@ -4,9 +4,8 @@ import axios from 'axios'
 import { useAuth } from '../auth/AuthContext'
 import Swal from 'sweetalert2'
 
-const API_URL = import.meta.env.VITE_API_URL || 'http://127.0.0.1:8000';
+const API_URL = process.env.REACT_APP_API_URL || 'http://127.0.0.1:8000';
 
-// Professional Toast Configuration
 const Toast = Swal.mixin({
   toast: true,
   position: 'top-end',
